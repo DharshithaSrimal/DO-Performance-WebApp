7edcf772f8fa7b0e66c4bd7e2ca8663e97a4084e
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2024-03-03 22:07:35 +0530

    date picker

--- do-performance/src/CalendarDatePicker.js
+++ do-performance/src/CalendarDatePicker.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
+import './DatePickerStyles.css';
 
 function CalendarDatePicker(){
     const [selectedDate, setSelectedDate] = useState(null);
     const handleDateChanage = (date) => {
+        console.log('dsadas '+ date);
         setSelectedDate(date);
     }
     return(
         <>
             <DatePicker
-            selected={selectedDate}
-            onChange={handleDateChanage}
-            dateFormat="YYYY/MM/DD"
+                selected={selectedDate}
+                onChange={handleDateChanage}
+                dateFormat="YYYY/MM/DD/"
             />
         </>
     );
